9daf5b1a95b7f48b439af919d65653e10268f26a
unknown <tansenananda@gmail.com>
2019-08-04 03:17:25 +0600

    Admin Panel

--- app/src/main/java/com/example/newdu/dnd/MainActivity.java
+++ app/src/main/java/com/example/newdu/dnd/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.newdu.dnd;
 
 import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startDbapp(View view){
+        new DbManager(this);
+        startActivity(new Intent(this,insertData.class));
     }
 }
